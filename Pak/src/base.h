f05e2827d1e59655686dda69bde279d1d6354ed9
LordSk <lordskelethom@gmail.com>
2020-07-15 19:59:46 +0200

    Compute xor block

--- Pak/src/base.h
+++ Pak/src/base.h
@@ -38,6 +38,7 @@ inline void __assertion_failed(const char* cond, const char* file, int line)
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
+typedef uint64_t u64;
 typedef int16_t i16;
 typedef int32_t i32;
 typedef int64_t i64;
